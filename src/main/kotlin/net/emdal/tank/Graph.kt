7128251f75e6fcb8ab26a18282a7caa0c97a78d8
kollstrom <e.kollstrom@gmail.com>
2019-10-29 15:07:58 +0100

    Change node graph methods to be member functions
    ... instead of extension functions

--- src/main/kotlin/net/emdal/tank/Graph.kt
+++ src/main/kotlin/net/emdal/tank/Graph.kt
@@ -1,3 +1,20 @@
 package net.emdal.tank
 
-data class Graph(val query: String = "", val aliases: List<Pair<String, Entity>> = emptyList())
+data class Graph(val query: String = "", val aliases: List<Pair<String, Entity>> = emptyList()) {
+  fun <T : Node> node(
+    alias: String = "",
+    node: T,
+    block: T.() -> String = { "" }
+  ): Graph = node(node, alias, block)
+
+  fun <T : Node> node(
+    node: T,
+    alias: String = "",
+    block: T.() -> String = { "" }
+  ): Graph {
+    return this.copy(
+      query = query + "($alias:${node.label} {${node.block()}})",
+      aliases = aliases + (alias to node)
+    )
+  }
+}
